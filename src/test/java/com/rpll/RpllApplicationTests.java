42589685c9473d13a966bc5f0c14e63e78881684
Nathaniel7144 <41036605+Nathaniel7144@users.noreply.github.com>
2020-03-17 20:33:41 +0700

    Spring project init, silahkan pull lalu download dependencynya.

---
+++ src/test/java/com/rpll/RpllApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rpll;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RpllApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
