e54d9ba19b4430c495ac04e584d68877403160ae
Jessica Cecilia <jessica.cecilia12soz@gmail.com>
2020-03-25 20:06:14 +0700

    Avatar and title change based on user occupation

--- src/script/login.js
+++ src/script/login.js
@@ -1,8 +1,27 @@
 user = {
-    name: "John",
+    name: "Alex",
     gender: 0,
-    position: ""
+    position: "Admin"
 } // will be accepted from server later
+
+TITLEANDPHOTO = {
+    Doctor: {
+        title: "Dr. "
+    },
+    Nurse: {
+        title: "Nurse "
+    },
+    General: {
+        0: {
+            title: "Mr. "
+        },
+        1: {
+            title: "Ms. "
+        }
+    }
+}
+
+ASSET = "../asset/";
 
 $(document).ready(function () {
     changeUIIfUserExists();
@@ -13,17 +32,53 @@ function changeUIIfUserExists() {
     // email -> password
     if (user !== undefined) {
         emailToPasswordInput();
+        user.position = generalizePosition(user.position);
+        displayUsername(
+            getTitle(user.gender, user.position) +
+            user.name
+        );
+        setAvatar(user.position, user.gender);
     }
 }
 
 function emailToPasswordInput() {
+    // change email to password input if user is not undefined
     $("#login__input").attr({
         type: "password",
         placeholder: "Password"
     });
-    $("#login__ic").attr({
-        src: "../asset/ic_lock.png",
-        width: "19px",
-        height: "19px"
-    });
+    $("#login__ic").attr("src", ASSET + "ic_lock.png");
+}
+
+function generalizePosition(position){
+    // function to change admin/HR/Cashier -> General
+    return ((!(position === "Doctor" || position === "Nurse")))?
+        "General" : position;
+}
+
+function getTitle(gender, position){
+    // Mr., Ms., ...
+    return getDataByPositionGender(
+        position, gender
+    )["title"];
+}
+
+function getDataByPositionGender(position, gender){
+    // Access TITLEANDPHOTO tree
+    return (position === "General")?
+        TITLEANDPHOTO[position][gender]:
+        TITLEANDPHOTO[position];
+}
+
+function displayUsername(name){
+    // method to display username if user has provided email
+    $("#login__name").text(name);
+}
+
+function setAvatar(position, gender){
+    // method that will set avatar
+    $("#login__avatar__img").attr(
+        "src", ASSET + position.toLowerCase() +
+        "_" + ((gender === 0)? "male" : "female") + ".jpg"
+    );
 }
