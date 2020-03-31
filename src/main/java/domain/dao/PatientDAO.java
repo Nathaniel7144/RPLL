3ac3fb9639284d84aa6108c725345aa446d70398
Nathaniel7144 <41036605+Nathaniel7144@users.noreply.github.com>
2020-03-31 23:11:23 +0700

    Ubah packaging, mengikuti clean architecture

---
+++ src/main/java/domain/dao/PatientDAO.java
@@ -0,0 +1,28 @@
+package domain.dao;
+
+import java.util.List;
+import domain.entity.Patient; 
+
+public class PatientDAO {
+
+	List<Patient> getAllPatient()
+	{
+		return null;
+	}
+
+    Patient findPatientById(int id)
+    {
+    	return null;
+    }
+
+    Patient savePatient(Patient patient)
+    {
+    	return null;
+    }
+
+    void deletePatientById(int id)
+    {
+    	
+    }
+	
+}
