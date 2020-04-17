c0e4425941480dbd97253e44b8ee9d91b76927e2
fadiljr <fadiljanuar44@gmail.com>
2020-04-17 23:28:20 +0700

    Finish database creation

--- database/ddl.sql
+++ database/ddl.sql
@@ -22,14 +22,96 @@ CREATE TABLE patient (
 );
 
 CREATE TABLE patientvisit(
+   id INT NOT NULL,
 	starttime DATETIME NOT NULL,
 	endtime DATETIME NOT NULL,
 	patient_id INT NOT NULL,
 	istreated BOOLEAN NOT NULL,
-	PRIMARY KEY (starttime, patient_id),
+	PRIMARY KEY (id),
 	FOREIGN KEY (patient_id) REFERENCES patient (id)
 );
 
+
+CREATE TABLE employee(
+   emp_id INT NOT NULL,
+   email VARCHAR(200) NOT NULL,
+   position VARCHAR(200) NOT NULL,
+   pwd CHAR(8) NOT NULL,
+   worksince DATETIME NOT NULL,
+   person_id INT,
+   PRIMARY KEY (emp_id),
+   FOREIGN KEY (person_id) REFERENCES person (id)
+);
+CREATE TABLE physician(
+   id INT NOT NULL,
+   specialist VARCHAR(200) NOT NULL,
+   emp_id INT,
+   PRIMARY KEY (id),
+   FOREIGN KEY (emp_id) REFERENCES employee(emp_id)
+);
+CREATE TABLE pharmacy(
+   id INT NOT NULL,
+   competency VARCHAR(200) NOT NULL,
+   emp_id INT,
+   PRIMARY KEY (id),
+   FOREIGN KEY (emp_id) REFERENCES employee(emp_id)
+);
+CREATE TABLE nurse(
+   id INT NOT NULL,
+   tipe VARCHAR(200) NOT NULL,
+   emp_id INT,
+   PRIMARY KEY (id),
+   FOREIGN KEY (emp_id) REFERENCES employee (emp_id)
+);
+CREATE TABLE nonmedical(
+   id INT NOT NULL,
+   dept VARCHAR(200) NOT NULL,
+   emp_id INT,
+   PRIMARY KEY (id),
+   FOREIGN KEY (emp_id) REFERENCES employee (emp_id)
+);
+CREATE TABLE vendor(
+   ven_id INT NOT NULL,
+   name VARCHAR (200) NOT NULL,
+   address VARCHAR (200) NOT NULL,
+   PRIMARY KEY (ven_id)
+);
+CREATE TABLE transaksi(
+   id INT NOT NULL,
+   tanggal DATETIME,
+   PRIMARY KEY(id)
+);
+CREATE TABLE ven_trans(
+   id INT NOT NULL,
+   ven_id INT,
+   trans_id INT,
+   PRIMARY KEY (id),
+   FOREIGN KEY (ven_id) REFERENCES vendor (ven_id),
+   FOREIGN KEY (trans_id) REFERENCES transaksi(id)
+);
+CREATE TABLE product(
+   id INT NOT NULL,
+   name VARCHAR(200) NOT NULL,
+   stock INT NOT NULL,
+   PRIMARY KEY(id)
+);
+CREATE TABLE prodtrans(
+   id INT NOT NULL,
+   prod_id INT NOT NULL,
+   trans_id INT NOT NULL,
+   PRIMARY KEY (id),
+   FOREIGN KEY (prod_id) REFERENCES product (id),
+   FOREIGN KEY (trans_id) REFERENCES transaksi(id)
+);
+CREATE TABLE payment(
+   id INT NOT NULL,
+   paychange DOUBLE NOT NULL,
+   patient_vist DATETIME,
+   payout DOUBLE NOT NULL,
+   patient_id INT NOT NULL,
+   PRIMARY KEY (id),
+   FOREIGN KEY (patient_id) REFERENCES patientvisit(id)
+);
 CREATE TABLE medicalrecord (
 	id INT NOT NULL,
 	problem VARCHAR(100) NOT NULL,
@@ -37,9 +119,10 @@ CREATE TABLE medicalrecord (
 	treatment VARCHAR(100) NULL,
 	patient_id INT NOT NULL,
 	physician_id INT NOT NULL,
-	patientvisit_time DATETIME NOT NULL,
-	PRIMARY KEY (id, patient_id) NOT NULL,
+	patvist_id INT NOT NULL,
+	PRIMARY KEY (id, patient_id),
 	FOREIGN KEY (patient_id) REFERENCES patient (id),
 	FOREIGN KEY (physician_id) REFERENCES physician (id),
-	FOREIGN KEY patientvisit_time REFERENCES patientvisit (starttime)
+	FOREIGN KEY (patvist_id) REFERENCES patientvisit (id)
 );
+
